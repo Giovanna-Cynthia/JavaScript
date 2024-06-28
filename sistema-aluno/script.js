5d2ffd02350b83e423a5c33f00571b9295ba31bd
Giovanna-Cynthia <giovannactnhia34@gmail.com>
2024-06-28 08:58:06 -0300

    Atualizações

--- sistema-aluno/script.js
+++ sistema-aluno/script.js
@@ -2,19 +2,38 @@
 const button = document.querySelector("button");
 const inputNota = document.querySelector("#nota");
 
-//Criando uma função
+//Criando uma função para limpar o campo
+function clearForm() {
+    //para limpar o campo depois que receber a mensagem(alerta)
+    //se colocar um texto, ele aparecerá depois que receber a mensagem(alerta)
+    inputNotaValue.value = "";
+}
+
+//Criando uma função anonima
 //Escutador de evento, quando clicar no botão ouvirá um evento
 //O alert é usado para aparecer alguma mensagem, quando apertar  no botão por exemplo
 //previnindo qualquer bugue do browser
 //.value valor que o usuario ta digitando(Aparece no console do inspecionar)
 button.addEventListener("click", function(event) {
-//alert("Bem-Vindo!");
-    event.preventDefault();
-    console.log(inputNota.value);
+//Alert("Bem-Vindo!");
 
-    if(inputNota.value >= 6 ){
-        console.log("Você está aprovado");
+    event.preventDefault();
+    const inputNotaValue = inputNota.value;
+    //console.log(inputNota.value);
+
+    //Validação de formulario
+    if(inputNotaValue === '' ) {
+        alert("Campo obrigatório!");
+        return false;
+    }
+
+    if(inputNotaValue >= 6 ){
+        alert("Você está aprovado");
     }else{
         alert("Você reprovou");
     }
+
+    //Chamando função de limpar campo
+    clearForm();
+
 });
